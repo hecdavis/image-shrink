c8765168375bce420e7a7bdbb23121fdc02ce0c2
Hector Davis <hector.davis94@gmail.com>
2022-03-11 20:24:10 -0300

    .prettierrc file added

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron');
 
-const isDev = process.env.NODE_ENV !== "production";
-const isMac = process.platform === "darwin";
-const isWin = process.platform === "win32";
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+const isWin = process.platform === 'win32';
 
 let mainWindow;
 
@@ -10,17 +10,17 @@ function createMainWindow() {
   mainWindow = new BrowserWindow({
     width: 500,
     height: 600,
-    title: "ImageShrink",
+    title: 'ImageShrink',
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
     resizable: isDev,
   });
   mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
 
-app.on("ready", createMainWindow);
+app.on('ready', createMainWindow);
 
 // Quit when all windows are closed
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   // On macOS it's common to apps and their menu bar to stay
   // active until the user quits explicitly with Cmd + Q
   if (!isMac) {
@@ -28,7 +28,7 @@ app.on("window-all-closed", () => {
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
